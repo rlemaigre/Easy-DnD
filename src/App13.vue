041f1d74e2a4f5e48a708651dc3dc2d0c3dcafd0
nicholaszuccarelli <nicholaszuccarelli@gmail.com>
2022-01-17 11:54:43 +1100

    Revert change in v1.20.0 breaking draggable element interaction

--- src/App13.vue
+++ src/App13.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <Drag v-for="n in items" :key="n" :data="n">
-      <img alt="Vue logo" src="https://vuejs.org/images/logo.png" width="25%" />
+      <img alt="Vue logo" src="https://vuejs.org/images/logo.png" width="25%" ondragstart="return false;" />
     </Drag>
     <Drop
         @drop="func"
-        style="width: 500px; height: 500px; background: #000"
+        style="width: 100%; max-width: 500px; height: 500px; background: #000"
     ></Drop>
   </div>
 </template>
@@ -18,7 +18,7 @@ export default {
   name: "App",
   data() {
     return {
-      items: [1, 2, 3, 4],
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     };
   },
   components: {
